c7cef73d41adf08fe6ccb9d913e9f0160b134bf8
Asmita Paudel <94975243+Asmitapaudel@users.noreply.github.com>
2022-07-26 16:33:50 +0545

    update

--- Assignment/Assignment9/students/action/save.php
+++ Assignment/Assignment9/students/action/save.php
@@ -1,3 +1,5 @@
+
+
 <?php 
 
 require_once "../../utils/db.php";
@@ -7,11 +9,7 @@ if (isset($_POST['save'])) {
     $tempname = $_FILES["profile"]["tmp_name"];
     $folder = "../image/" . $filename;
 
-	if (move_uploaded_file($tempname, $folder)) {
-        echo "<h3>  Image uploaded successfully!</h3>";
-    // } else {
-    //     echo "<h3>  Failed to upload image!</h3>";
-    // }
+
 
 $name = $_POST['name'] ?? '';
 $email = $_POST['email'] ?? '';
@@ -24,7 +22,16 @@ $hobbies = implode(",", $_POST['hobbies'] ?? []);
 $nationality = $_POST['nationality'] ?? 'NP';
 
 $sql = "INSERT INTO students (name, email, password, dob, favourite_color, weight, gender, hobbies, nationality,profile)
-VALUES ('$name', '$email', '$password', '$dob', '$favourite_color', $weight, '$gender', '$hobbies', '$nationality','$folder')";
+VALUES ('$name', '$email', '$password', '$dob', '$favourite_color', $weight, '$gender', '$hobbies', '$nationality','$filename')";
+// die($sql);
+
+mysqli_query($conn, $sql);
+    
+if (move_uploaded_file($tempname, $folder)) {
+    echo "<h3>  Image uploaded successfully!</h3>";
+} else {
+    echo "<h3>  Failed to upload image!</h3>";
+}
 
 if($conn->query($sql) == TRUE) {
 	header("location:../?success=inserted successfully");
